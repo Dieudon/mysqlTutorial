064bd42e242226062c7c67820849b09ed7c878c3
degstorf <d.egstorf@publitas.com>
2013-06-13 17:40:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MysqlTutorial::Application.config.secret_token = '570b9ec4125dfddaaee44ff32745f05b7bf3129013860666a6d35b19784434d6eae73ae2e8043512434397e7fa46d56e46139c065fdb8a106fa511d912dae6ce'
